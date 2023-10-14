80188e15bc6744f9bf9010ff72f75ebad219fff3
Shinya Kato <contact@shinyakato.dev>
2023-10-14 17:28:42 +0900

    find untranslated keys

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # l10n
+
 Community managed translations for deck.blue are here!
 
-# How to help?
+## How to help?
 
-If you just want to update an existing language, just edit the file based on the original `strings_en.arb` and `untranslated.txt` files. 
+If you just want to update an existing language, just edit the file based on the original `strings_en.arb` and `untranslated.json` files.
 
- - The `strings_en.arb` file will have a description (that can also be updated) explaining wher that specific text is used within the app.
- - The `untranslated.txt` files will group all untranslated text between all languages, please be sure to check that first!
+- The `strings_en.arb` file will have a description (that can also be updated) explaining wher that specific text is used within the app.
+- The `untranslated.json` files will group all untranslated text between all languages, please be sure to check that first!
 
-# Adding a new language
+## Adding a new language
 
 Start by copying the full `strings_en.arb` file, then rename it to have the language tag based on the [IANA list](https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry). For example, to add French, you would rename the file to `strings_fr.arb`. If it's a language that needs the full tag, such as Spanish (es) vs Latin America Spanish (es-419), the files would be `strings_es.arb` and `strings_es_419.arb` respectively.
 
-# Reviewing
+## Reviewing
 
 Since this is a collaborative repository, I turned on the 2 approval requirement for new languages, and 1 approval requirement for updating an existing language. Please help me help you to help users in your language by reviewing new additions and making sure it looks and feels correct for many users to see.
---
+++ scripts/update_untranslated.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+import 'dart:io';
+
+const _basePath = './strings';
+const _templateFileName = 'strings_en.arb';
+const _untranslatedFileName = 'untranslated.json';
+const _descriptionSymbol = '@';
+
+const _jsonConverter = JsonEncoder.withIndent('  ');
+
+void main() {
+  final templateFile = File('$_basePath/$_templateFileName');
+
+  final untranslated = <String, dynamic>{};
+  for (final file in Directory(_basePath).listSync()) {
+    if (file.path.endsWith(_templateFileName)) continue;
+
+    final targetFile = File(file.path);
+
+    final Map<String, dynamic> templateStrings =
+        jsonDecode(templateFile.readAsStringSync());
+    final Map<String, dynamic> targetStrings =
+        jsonDecode(targetFile.readAsStringSync());
+
+    final keys = <String>[];
+    templateStrings.forEach((key, _) {
+      if (!targetStrings.containsKey(key) &&
+          !key.startsWith(_descriptionSymbol)) {
+        keys.add(key);
+      }
+    });
+
+    untranslated[_getLangCode(targetFile)] = keys;
+  }
+
+  File(_untranslatedFileName).writeAsStringSync(
+    _jsonConverter.convert(untranslated),
+  );
+}
+
+/// Extracts the language code from the filename of a given [file].
+///
+/// This function performs the following steps to derive the language code:
+///   1. Extracts the filename from the file's path.
+///   2. Splits the filename using the dot ('.') as a delimiter and takes the first part.
+///   3. Splits the result using the underscore ('_') as a delimiter and takes the second part.
+///
+/// ## Parameters:
+/// - [file]: The file from which the language code is to be extracted.
+///
+/// ## Returns:
+/// A `String` representing the extracted language code.
+String _getLangCode(final File file) =>
+    file.path.split('/').last.split('.').first.split('_')[1];
