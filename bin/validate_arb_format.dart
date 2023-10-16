1a99ee9216ee7366605b3b8d0d6b85272b84fd16
Shinya Kato <contact@shinyakato.dev>
2023-10-16 14:29:00 +0900

    validate arb format

---
+++ bin/validate_arb_format.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+import 'dart:io';
+
+const _basePath = './strings';
+const _templateFileName = 'strings_en.arb';
+
+void main(List<String> args) {
+  final templateFile = File('$_basePath/$_templateFileName');
+
+  if (!templateFile.existsSync()) {
+    throw FormatException('Do not remove "${templateFile.path}".');
+  }
+
+  final Map<String, dynamic> templateJson = jsonDecode(
+    templateFile.readAsStringSync(),
+  );
+
+  for (final file in Directory(_basePath).listSync()) {
+    if (!file.path.endsWith('.arb')) continue;
+
+    final arb = File(file.path);
+
+    try {
+      jsonDecode(arb.readAsStringSync());
+    } catch (_) {
+      throw FormatException('"${arb.path}" has not valid JSON.');
+    }
+
+    if (file.path == templateFile.path) continue;
+
+    final Map<String, dynamic> arbJson = jsonDecode(arb.readAsStringSync());
+
+    arbJson.forEach((key, _) {
+      if (!templateJson.containsKey(key)) {
+        throw FormatException(
+            'The "$key" in "${arb.path}" does not exist in the template.');
+      }
+    });
+  }
+}
