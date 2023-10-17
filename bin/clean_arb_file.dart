31475fa9879a9eda5c31982294fd874674d381e3
Shinya Kato <contact@shinyakato.dev>
2023-10-17 14:43:39 +0900

    add `clean` event
    Clean the arb files based on the template file when merging the latest template into l10n and when merging the contributor's edits.
    
    - Delete keys that do not exist in the template file.
    - Sorting based on the order of keys in the template file.

---
+++ bin/clean_arb_file.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'dart:io';
+
+const _templateFileName = 'strings_en.arb';
+
+const _jsonConverter = JsonEncoder.withIndent('  ');
+
+void main(List<String> args) {
+  final templateFile = File('strings/$_templateFileName');
+  final Map<String, dynamic> templateJson =
+      jsonDecode(templateFile.readAsStringSync());
+
+  for (final file in Directory('strings').listSync()) {
+    if (file.path.endsWith(_templateFileName)) continue;
+
+    final arbFile = File(file.path);
+    final Map<String, dynamic> arbJson = jsonDecode(arbFile.readAsStringSync());
+
+    final cleanedArb = <String, dynamic>{};
+    templateJson.forEach((key, value) {
+      if (arbJson.containsKey(key)) {
+        cleanedArb[key] = arbJson[key];
+      }
+    });
+
+    arbFile.writeAsStringSync(_jsonConverter.convert(cleanedArb));
+  }
+}
